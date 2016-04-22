306c38a12b2927f7fa103159020860190da0a585
chrisotto <cotto@yahoo.com>
2016-04-21 22:13:09 -0700

    Add pseudocode for phase 0 challenge 5.3 release 3

---
+++ ruby/hashes.rb
@@ -0,0 +1,14 @@
+# Create a method to collect the value for a client detail (key)
+  # prompt the user to input a value for the key
+  # lookup the appropriate data type for that key
+  # convert the value to the appropriate data type
+  # add the value for the key to the client_details hash
+# Create a client_details hash with default keys and nil values
+# Create a client_details_data_types hash with the same default keys and appropriate data type values
+# Collect the value for each client detail (key)
+# Print the client_details hash
+# Prompt the user to update a client detail by typing its key (or "none" to skip)
+# If the user types a key, collect the new value for that client detail (key)
+# Elsif the user types anything other than "none"
+  # Leave error handling for another day
+# Print the hash
