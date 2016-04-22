07b74d9c13229fad96cc31e209f92be668d476da
chrisotto <cotto@yahoo.com>
2016-04-21 22:16:59 -0700

    Init hashes

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -4,7 +4,21 @@
   # convert the value to the appropriate data type
   # add the value for the key to the client_details hash
 # Create a client_details hash with default keys and nil values
+client_details = {
+  client_name: nil,
+  age: nil,
+  number_of_children: nil,
+  decor_theme: nil,
+  likes_flowers: nil
+}
 # Create a client_details_data_types hash with the same default keys and appropriate data type values
+client_details_data_types = {
+  client_name: String,
+  age: Integer,
+  number_of_children: Integer,
+  decor_theme: String,
+  likes_flowers: "Boolean"
+}
 # Collect the value for each client detail (key)
 # Print the client_details hash
 # Prompt the user to update a client detail by typing its key (or "none" to skip)
