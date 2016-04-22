8b4921e38eef5f6a71c1d496eeb0354b22bc4fae
chrisotto <cotto@yahoo.com>
2016-04-22 16:46:40 -0700

    Add release 1

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -6,3 +6,35 @@ def test_method
 end
 
 test_method {|two| puts "This is our #{two} test method!"}
+
+movies = [
+  "The Hangover",
+  "The Jungle Book",
+  "Batman vs. Superman"
+  ]
+
+actors = {
+  "Justin Bartha" => "Doug",
+  "Mike Tyson" => "Mike Tyson",
+  "The Bear" => "Bill Murray",
+  "Ben Affleck" => "Batman"
+  }
+
+movies.each do
+  |movie|
+  puts "Movie: #{movie}"
+end
+
+actors.each do
+  |actor, role|
+  puts "Starring #{actor} as #{role}"
+end
+
+puts movies
+movies.map! do
+  |movie|
+  movie = movie.upcase
+  puts "Movie: #{movie}"
+  movie
+end
+puts movies
