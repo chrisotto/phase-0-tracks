dc8742706d8cb4df40a761e6c8172728f66e7628
chrisotto <cotto@yahoo.com>
2016-04-22 14:55:35 -0700

    Fix initial data structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,40 +1,40 @@
-'san francisco' = {
-  'mission': {
+san_francisco = {
+  mission: {
     restaurants: [
       "Lolo",
       "Hoffman's Grill & Rotisserie",
       "20 Spot"
-    ]
+    ],
     bars: [
       "Shotwell's",
       "Trick Dog",
       "24th Street Bar"
     ]
-  }
-  'noe valley': {
+  },
+  noe_valley: {
     restaurants: [
       "Le Zinc",
       "Patxi's"
     ]
-  }
-  'potrero hill': {
+  },
+  potrero_hill: {
     restaurants: [
       "Chez Maman",
       "Plow",
       "Papito Potrero Hill"
     ]
-  }
-  'soma': {
+  },
+  soma: {
     restaurants: [
       "Garaje",
       "HRD"
-    ]
+    ],
     bars: [
       "Alchemist Bar & Lounge",
       "Lord George"
     ]
-  }
-  'fidi': {}
-  'pacific heights': {}
-  'sunset': {}
+  },
+  fidi: {},
+  pacific_heights: {},
+  sunset: {}
 }
