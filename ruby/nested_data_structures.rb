f2d0b79b68c73078ddbb047d81a62769c8531c6a
chrisotto <cotto@yahoo.com>
2016-04-22 14:45:14 -0700

    Build initial data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,40 @@
+'san francisco' = {
+  'mission': {
+    restaurants: [
+      "Lolo",
+      "Hoffman's Grill & Rotisserie",
+      "20 Spot"
+    ]
+    bars: [
+      "Shotwell's",
+      "Trick Dog",
+      "24th Street Bar"
+    ]
+  }
+  'noe valley': {
+    restaurants: [
+      "Le Zinc",
+      "Patxi's"
+    ]
+  }
+  'potrero hill': {
+    restaurants: [
+      "Chez Maman",
+      "Plow",
+      "Papito Potrero Hill"
+    ]
+  }
+  'soma': {
+    restaurants: [
+      "Garaje",
+      "HRD"
+    ]
+    bars: [
+      "Alchemist Bar & Lounge",
+      "Lord George"
+    ]
+  }
+  'fidi': {}
+  'pacific heights': {}
+  'sunset': {}
+}
