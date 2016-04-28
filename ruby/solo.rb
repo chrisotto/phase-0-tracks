557ab3e2e37f28780ff1393afd75c363c9fc29a6
chrisotto <cotto@yahoo.com>
2016-04-28 00:52:10 -0700

    Add Car class

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -5,3 +5,43 @@
   # Define a method to rev the engine that prints "VROOM!  VROOM!"
   # Define a method to honk the horn with a parameter for the number of times it should print "HONK!"
   # Define a method to drive with a parameter for the location that prints "Driving to <location>"
+class Car
+  attr_accessor :color
+  attr_reader :make, :model, :year
+
+  def initialize(year, make, model, color)
+    puts "Initializing Car instance ..."
+    @color = color
+    print @color, " "
+    @year = year
+    print @year, " "
+    @make = make
+    print @make, " "
+    @model = model
+    puts @model
+  end
+
+  def rev_engine
+    puts "VROOM!  VROOM!"
+  end
+
+  def honk(i)
+    i.times {print "HONK!  "}  # TBD:  This prints two extra spaces at the end of the line
+    puts
+  end
+
+  def drive(destination)
+    puts "Driving to #{destination}."
+  end
+
+end
+
+# Diagnostic driver code
+car = Car.new(1987, "Honda", "Prelude", "Red")
+car.rev_engine
+car.honk(2)
+car.drive("Disneyland")
+puts "Car color:  #{car.color}"
+puts "Car year:  #{car.year}"
+puts "Car make:  #{car.make}"
+puts "Car model:  #{car.model}"
