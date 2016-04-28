c43364ab6bd92dc78ef52a00833e5dd2e19d8267
chrisotto <cotto@yahoo.com>
2016-04-28 03:01:06 -0700

    Add Student and Coach classes and Shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,38 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#   def self.yelling_happily(words)
+#     words + "!!!" + "  Yippee!"
+#   end
+# end
+# # Driver Code
+# puts Shout.yell_angrily("This isn't working")
+# puts Shout.yelling_happily("Eureka")
+
 module Shout
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
-  def self.yelling_happily(words)
+  def yelling_happily(words)
     words + "!!!" + "  Yippee!"
   end
 end
+
+class Student
+  include Shout
+end
+
+class Coach
+  include Shout
+end
+
 # Driver Code
-puts Shout.yell_angrily("This isn't working")
-puts Shout.yelling_happily("Eureka")
+student = Student.new
+puts "The student says:"
+puts student.yell_angrily("This isn't working")
+puts student.yelling_happily("Eureka")
+coach = Coach.new
+puts "The coach says:"
+puts coach.yell_angrily("Get your act together")
+puts coach.yelling_happily("We win")
