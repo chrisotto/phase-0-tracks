83d27ee4cb8628d4a33c1548f111e6d8a51180ed
chrisotto <cotto@yahoo.com>
2016-04-28 00:02:50 -0700

    Add pseudocode for Car class

---
+++ ruby/solo.rb
@@ -0,0 +1,7 @@
+# Define a Car class
+  # Initialize with required parameters for make, model, year and color
+  # Allow all attributes to be read
+  # Allow color to be written
+  # Define a method to rev the engine that prints "VROOM!  VROOM!"
+  # Define a method to honk the horn with a parameter for the number of times it should print "HONK!"
+  # Define a method to drive with a parameter for the location that prints "Driving to <location>"
