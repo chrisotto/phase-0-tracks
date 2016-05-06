0abbae84bf3b97edfa68d366677b02d25a9c9def
chrisotto <cotto@yahoo.com>
2016-05-05 17:45:18 -0700

    Add pseudocode for reverse string method

---
+++ js/explore.js
@@ -0,0 +1,5 @@
+// Add a function that takes a string as a parameter and reverses the string
+//   Create an empty return string
+//   For each character in the given string
+//     Prepend the character to the return string
+//   Return the return string
