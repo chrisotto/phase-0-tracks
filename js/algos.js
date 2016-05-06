3aff3b13f6aa47b535ac3ca6f3cbb2ecb44e43c9
chrisotto <cotto@yahoo.com>
2016-05-06 16:13:41 -0700

    Add pseudocode for longestItem function

---
+++ js/algos.js
@@ -0,0 +1,7 @@
+// Add a function that takes an array of words or phrases and returns the longest word or phrase in the array
+//   Create an empty string variable for the return value
+//   For each item in the argument array
+//     If the length of the item is greater than the length of the return value
+//       Replace the return value with the item
+//     TBD:  Requirements are unclear with regard to how to handle multiple words or phrases that tie for the longest word or phrase.  This function currently returns only the first of the longest words and phrases in the array.
+//   Return the return value
